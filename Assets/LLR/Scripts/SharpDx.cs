5518b95f0a98d24e2df9724cfd740dd09a6c0339
ousttrue <ousttrue@gmail.com>
2017-04-10 22:48:02 +0900

    init

---
+++ Assets/LLR/Scripts/SharpDx.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+public class SharpDx : MonoBehaviour
+{
+    void Start()
+    {
+        var tex = new Texture2D(640, 480);
+
+        using (var t = new SharpDX.Direct3D11.Texture2D(tex.GetNativeTexturePtr()))
+        {
+            var desc=t.Description;
+            Debug.Log(desc.Width);
+            int a = 0;
+        }
+    }
+}
